23fc1ef028570121638c293c38dc87fba80e2a9c
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2012-05-22 04:22:35 +0100

    Clave secreta del proyecto sacada a archivo de configuración de instalación.

--- settings_local.py
+++ settings_local.py
@@ -20,4 +20,20 @@ DATABASES = {
     }
 }
 
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# On Unix systems, a value of None will cause Django to use the same
+# timezone as the operating system.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'Atlantic/Canary'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'es-es'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'vwz(3e9-zuqf)l-&amp;6srbf7xk*gklc^*ed#=m7=lcq95mkvresz'
+
 
