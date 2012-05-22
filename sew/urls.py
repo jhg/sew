1111bf9ea1e5c2edd0bcf346493af5cefe70b095
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2012-05-22 04:07:23 +0100

    Configurados archivos estaticos. Configuración concreta de instalación sacada a archivo individual.

--- sew/urls.py
+++ sew/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -19,3 +20,11 @@ urlpatterns = patterns('',
     url(r'^blog/', include('blogs.urls')),
     url(r'^$', index),
 )
+
+# No usar en produccion, servirlos configurando el servidor HTTP
+urlpatterns += patterns('',
+    (r'^archivos-estaticos/(?P<path>.*)$',
+        'django.views.static.serve',
+        {'document_root': settings.STATIC_ROOT, 'show_indexes': True,}),
+)
+
