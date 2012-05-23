cd0660ce294d72d0a232d9ce577dfeeb79246e61
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2012-05-23 02:32:51 +0100

    Modificado modelo de blog para que los autores sean usuarios del sistema.

--- blogs/models.py
+++ blogs/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Blog(models.Model):
     titulo = models.CharField(max_length=32, default="")
-    subdirectoriourl = models.CharField(max_length=42, default="")
+    suburl = models.CharField(max_length=42, default="")
+    autores = models.ManyToManyField(User)
     descripcion = models.TextField(max_length=256, default="")
     def __unicode__(self):
         return self.titulo
