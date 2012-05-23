aebc9fb43ad0adf7f88328a049152671b82af779
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2012-05-23 02:11:15 +0100

    Avanzando en el diseño del modelo de blog. Ahora con descripción y URL.

--- blogs/models.py
+++ blogs/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
-class Blogs(models.Model):
-    titulo = models.CharField(max_length=32)
+class Blog(models.Model):
+    titulo = models.CharField(max_length=32, default="")
+    subdirectoriourl = models.CharField(max_length=42, default="")
+    descripcion = models.TextField(max_length=256, default="")
+    def __unicode__(self):
+        return self.titulo
 
