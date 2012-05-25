2c31ef35f46c9fc28588d8dfe16cdec37de3e814
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2012-05-25 03:03:19 +0100

    Creada vista para mostrar un blog y comenzando mostrando la descripcion.

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('blogs.views',
-    url(r'^$', 'index'),
+    url(r'^$', 'index', name="blogs-url"),
+    url(r'^(?P<urlblog>\w+)/$', 'blog'),
 )
--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,19 @@
 from blogs.models import Blog
 from django.shortcuts import render_to_response
+from settings_local import HOST
 
 def index(request):
-    return render_to_response('construccion.htm', {})
+    blogs = Blog.objects.all()
+    num_blogs = len(blogs)
+    return render_to_response('blogs/index.htm', locals())
+
+def blog(request, urlblog):
+    try:
+        blog = Blog.objects.get(url=urlblog)
+        return render_to_response('blogs/blog_index.htm', locals())
+    except:
+        blogs = Blog.objects.all()
+        num_blogs = len(blogs)
+        host = HOST.strip("/")
+        return render_to_response('blogs/blog_inexistente.htm', locals())
 
