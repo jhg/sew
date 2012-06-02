63a1c8b4de0195d61071d1eb5b6771c6916e49f9
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2012-06-02 03:30:20 +0100

    Sobreescrito username de Auth de Django para nombres de usuario más largos.

--- sew/models.py
+++ sew/models.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
 
-# Sobreescribimos el campo de email para soptar al longitud total posible
+# Sobreescribiendo el campo username para nombres de usuario de 254 caracteres
+User.username = models.CharField(_('username'), max_length=254, unique=True,
+        help_text=_('Required. 254 characters or fewer. Letters, numbers and '
+                    '@/./+/-/_ characters'))
+
+# Sobreescribimos el campo de email para usar la longitud total posible
 User.email = models.EmailField(_('e-mail address'), blank=False, max_length=254)
 
