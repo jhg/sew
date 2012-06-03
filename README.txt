5538ce57caecf7f01c25d239a660368c9c5363bd
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2012-06-03 03:42:07 +0100

    Indicados requerimientos para usar django-extensions.

--- README.txt
+++ README.txt
@@ -1,6 +1,9 @@
 Dependencias:
+ Instalación con Pypi recomendada:
  - south
  - django-social-auth
  - django-extensions
+ Instalación del paquete python en sus sistema recomendada:
+ - pygraphviz
 
 LEEME.txt es un enlace simbolico a README.txt
--- sew/settings.py
+++ sew/settings.py
@@ -134,6 +134,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'django_extensions',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'blogs',
