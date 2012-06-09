810c0f404e8aa98bf8999d5237621f57a7b2e1eb
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2012-06-09 04:47:52 +0100

    Controlada variable peligrosa en la paginación mejorada de paginacionhc.

--- paginacionhc/PaginacionMejorada.py
+++ paginacionhc/PaginacionMejorada.py
@@ -1,5 +1,6 @@
 
 def accesos_directos_rango (actual, total, numero_accesos):
+    if numero_accesos < 1: numero_accesos = 1
     inicial = actual - numero_accesos
     if inicial < 1: inicial = 1
     final = actual + numero_accesos
