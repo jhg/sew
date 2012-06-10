ffb047fdd0b5999ab78b2e6f918167079cb12d23
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2012-06-10 20:13:14 +0100

    Control de blogs publicados y bloqueados añadido en la vista.

--- blogs/views.py
+++ blogs/views.py
@@ -8,37 +8,44 @@ from settings_local import HOST
 from blogs.models import Blog
 
 def index(request):
-    blogs = Blog.objects.all()
+    blogs = Blog.objects.filter(publicado=True, bloqueado=False)
     num_blogs = len(blogs)
     return render_to_response('blogs/index.htm', locals())
 
 def blog(request, urlblog):
     try:
         blog = Blog.objects.get(url=urlblog) # Encontramos el blog por su url
-        # Extraemos los articulos publicados en este blog y los ordenamos
-        articulos = blog.articulo_set.filter(publicado=True)
-        articulos = articulos.order_by("publicacion").reverse()
-        # Paginacion de los articulos
-        paginacion = Paginator(articulos, blog.articulos_por_pagina)
-        try:
-            pagina = int(request.GET.get('pagina'))
-        except:
-            pagina = 1
-        anteriores_pag, posteriores_pag = accesos_directos_rango(pagina,
-            paginacion.num_pages, blog.accesos_directos_paginacion)
-        try:
-            articulos = paginacion.page(pagina)
-        except PageNotAnInteger:
-            articulos = paginacion.page(1)
-        except EmptyPage:
-            articulos = paginacion.page(paginacion.num_pages)
-        # Usamos la propia plantilla del blog
-        plantilla = Template('{% extends "blogs/blog_index.htm" %}\n'
-            +blog.plantilla)
-        return HttpResponse(plantilla.render(Context(locals())))
+        if blog.bloqueado:
+            blogs = Blog.objects.filter(publicado=True, bloqueado=False)
+            num_blogs = len(blogs)
+            host = HOST.strip("/")
+            return render_to_response('blogs/blog_bloqueado.htm', locals())
+        elif blog.publicado:
+            # Extraemos los articulos publicados en este blog y los ordenamos
+            articulos = blog.articulo_set.filter(publicado=True)
+            articulos = articulos.order_by("publicacion").reverse()
+            # Paginacion de los articulos
+            paginacion = Paginator(articulos, blog.articulos_por_pagina)
+            try:
+                pagina = int(request.GET.get('pagina'))
+            except:
+                pagina = 1
+            anteriores_pag, posteriores_pag = accesos_directos_rango(pagina,
+                paginacion.num_pages, blog.accesos_directos_paginacion)
+            try:
+                articulos = paginacion.page(pagina)
+            except PageNotAnInteger:
+                articulos = paginacion.page(1)
+            except EmptyPage:
+                articulos = paginacion.page(paginacion.num_pages)
+            # Usamos la propia plantilla del blog
+            plantilla = Template('{% extends "blogs/blog_index.htm" %}\n'
+                +blog.plantilla)
+            return HttpResponse(plantilla.render(Context(locals())))
     except:
-        blogs = Blog.objects.all()
-        num_blogs = len(blogs)
-        host = HOST.strip("/")
-        return render_to_response('blogs/blog_inexistente.htm', locals())
+        pass
+    blogs = Blog.objects.filter(publicado=True, bloqueado=False)
+    num_blogs = len(blogs)
+    host = HOST.strip("/")
+    return render_to_response('blogs/blog_inexistente.htm', locals())
 
