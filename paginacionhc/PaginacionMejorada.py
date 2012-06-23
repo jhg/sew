2e19814f6483052ace8e724a43d6c5fd984e629d
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2012-06-23 12:02:35 +0100

    Añadida cadena de documentación a paginacionhc.

--- paginacionhc/PaginacionMejorada.py
+++ paginacionhc/PaginacionMejorada.py
@@ -1,4 +1,45 @@
 #-*- coding: UTF-8 -*-
+"""
+    accesos_directos_rango(actual, total, numero_accesos)
+        Un ejemplo de uso seria:
+            ant_pag, pos_pag = accesos_directos_rango(20, 100, 5)
+        Y podriamos, si por ejemplo lo usamos para paginar articulos de
+        un blog, usar este código en la plantilla:
+            {% block paginacion %}
+              {% if articulos.has_other_pages %}
+              <div class="paginacion">
+                {% if articulos.has_previous %}
+                  <a href="?pagina=1">primera</a>
+                  <a href="?pagina={{ articulos.previous_page_number }}">
+                    &lt;
+                  </a>
+                  {% for anterior in ant_pag %}
+                    <a href="?pagina={{ anterior }}">{{ anterior }}</a>
+                  {% endfor %}
+                {% endif %}
+                <span class="pagina-actual">
+                  {{ articulos.number }}
+                </span>
+                {% if articulos.has_next %}
+                  {% for posterior in pos_pag %}
+                    <a href="?pagina={{ posterior }}">{{ posterior }}</a>
+                  {% endfor %}
+                  <a href="?pagina={{ articulos.next_page_number }}">
+                    &gt;
+                  </a>
+                  <a href="?pagina={{ articulos.paginator.num_pages }}">
+                    ultima
+                  </a>
+                {% endif %}
+              </div>
+              {% endif %}
+            {% endblock %}
+        Y podemos quitar el subrayado de los enlaces con el CSS:
+            .paginacion a
+              {
+                text-decoration:none;
+              }
+"""
 
 
 def accesos_directos_rango(actual, total, numero_accesos):
