1d9bfb84e33257421bb1eee350606d4b74ee69d0
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2012-07-10 05:15:17 +0100

    Añadido ID de sitio para desarrollo del soporte de multiples dominios.

--- sew/views.py
+++ sew/views.py
@@ -1,6 +1,8 @@
 from blogs.models import Blog
 from django.shortcuts import render_to_response
+from django.conf import settings
 
 
 def index(request):
-    return render_to_response('construccion.htm', {})
+    idsite = settings.SITE_ID
+    return render_to_response('construccion.htm', {"idsite": idsite})
