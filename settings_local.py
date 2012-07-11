67ae3f39e4b4cd80f8fd03f55b127d635a21e648
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2012-07-11 01:34:10 +0100

    Segunda parte de la refactorización delicada de código de SEW en general.

--- settings_local.py
+++ settings_local.py
@@ -45,50 +45,6 @@ LANGUAGE_CODE = 'es-es'
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'vwz(3e9-zuqf)l-&amp;6srbf7xk*gklc^*ed#=m7=lcq95mkvresz'
 
-# Django Social Auth: OAuth Keys, availabel from:
-#    http://www.facebook.com/developers/createapp.php
-#    https://developer.yahoo.com/dashboard/createKey.html
-#    https://www.google.com/accounts/ManageDomains
-#    http://twitter.com/oauth_clients
-
-# Aplicacion Twitter para pruebas mantenida por
-#  Jesús Hernández Gormaz <jhg.jesus@gmail.com>
-# Nombre de la aplicación: SEW Test
-# Descripcion: Pruebas en el desarrollo de SEW
-# Web: http://127.0.0.1:8000/
-# Acceso: Solo lectura
-# URL de llamada atras: http://127.0.0.1:8000/admin/
-# Consumer key: WgC13BNFq9ubWLuzVxR3yA
-# Consumer secret: FDJiopJJCPyEqhxikvWGm2PgUzJql7pY2ENt7cbOF0
-TWITTER_CONSUMER_KEY = 'WgC13BNFq9ubWLuzVxR3yA'
-TWITTER_CONSUMER_SECRET = 'FDJiopJJCPyEqhxikvWGm2PgUzJql7pY2ENt7cbOF0'
-FACEBOOK_APP_ID = ''
-FACEBOOK_API_SECRET = ''
-DROPBOX_APP_ID = ''
-DROPBOX_API_SECRET = ''
-EVERNOTE_CONSUMER_KEY = ''
-EVERNOTE_CONSUMER_SECRET = ''
-LINKEDIN_CONSUMER_KEY = ''
-LINKEDIN_CONSUMER_SECRET = ''
-ORKUT_CONSUMER_KEY = ''
-ORKUT_CONSUMER_SECRET = ''
-GITHUB_APP_ID = ''
-GITHUB_API_SECRET = ''
-GOOGLE_CONSUMER_KEY = ''
-GOOGLE_CONSUMER_SECRET = ''
-GOOGLE_OAUTH2_CLIENT_ID = ''
-GOOGLE_OAUTH2_CLIENT_SECRET = ''
-FOURSQUARE_CONSUMER_KEY = ''
-FOURSQUARE_CONSUMER_SECRET = ''
-VK_APP_ID = ''
-VK_API_SECRET = ''
-LIVE_CLIENT_ID = ''
-LIVE_CLIENT_SECRET = ''
-SKYROCK_CONSUMER_KEY = ''
-SKYROCK_CONSUMER_SECRET = ''
-YAHOO_CONSUMER_KEY = ''
-YAHOO_CONSUMER_SECRET = ''
-
 # Dominio predeterminado del servidor
 #  Ejemplo: '127.0.0.1:8000'
 DEFAULT_HOST = '127.0.0.1:8000'
