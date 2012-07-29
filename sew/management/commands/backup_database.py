6b919112a359faaed9827f2367d55a17a5bb0461
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2012-07-29 13:33:01 +0100

    Refactorización de backup_database y agregados comentarios.

--- sew/management/commands/backup_database.py
+++ sew/management/commands/backup_database.py
@@ -10,8 +10,11 @@ class Command(BaseCommand):
     args = "[database]"
 
     def handle(self, *args, **options):
+        # Extraemos la configuración de la base de datos indicada
         db_settings = DATABASES[args[0]]
+        # Filtramos el tipo a partir del motor Django usado
         db_type = db_settings['ENGINE'].split('.')[-1].split('_')[0]
+        # Formamos el nombre de archivo de respaldo con el nombre de la BD
         db_file = '/' + args[0] + '.sql'
         # Comando de exportación para SQLite3
         if db_type == 'sqlite3':
@@ -19,16 +22,14 @@ class Command(BaseCommand):
                 settings.PRIVATE_ROOT + db_file
         # Comando de exportación para PostgreSQL
         elif db_type == 'postgresql':
-            db_command = 'PGUSER=' + db_settings['USER'] + \
-                ' PGPASSWORD=' + db_settings['PASSWORD'] + \
-                ' pg_dump ' + db_settings['NAME'] + ' > ' + \
-                settings.PRIVATE_ROOT + db_file
+            db_command = 'PGUSER=' + db_settings['USER'] + ' PGPASSWORD=' + \
+                db_settings['PASSWORD'] + ' pg_dump ' + db_settings['NAME'] + \
+                ' > ' + settings.PRIVATE_ROOT + db_file
         # Comando de exportación para MySQL
         elif db_type == 'mysql':
             db_command = 'mysqldump --opt --user=' + db_settings['USER'] + \
                 ' --password=' + db_settings['PASSWORD'] + \
-                db_settings['NAME'] + ' > ' + \
-                settings.PRIVATE_ROOT + db_file
+                db_settings['NAME'] + ' > ' + settings.PRIVATE_ROOT + db_file
         # Mantenemos el respaldo anterior con la extensión .old
         os.system('rm -f ' + settings.PRIVATE_ROOT + db_file + '.old')
         try:
