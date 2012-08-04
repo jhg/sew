63d465bc70ee7a5d4d1efedf05fc1c7b52b3096a
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2012-08-04 20:54:46 +0100

    Usado paquete Zip para importar objetos de blog.

--- blogs/management/commands/objetos_blog.py
+++ blogs/management/commands/objetos_blog.py
@@ -2,8 +2,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from settings_local import PROJECT_DIR
 from blogs.models import ObjetoBlog
-import zipfile
-from sew.ConfiguracionXML import ConfiguracionXML
+from sew.PaqueteZIP import PaqueteZIP
 
 
 class Command(BaseCommand):
@@ -11,20 +10,23 @@ class Command(BaseCommand):
     args = "[archivo]"
 
     def handle(self, *args, **options):
-        if nombre_nuevo != '':
+        paquete = PaqueteZIP(args[0])
+        if paquete.configuracion('nombre') != '' \
+            and paquete.configuracion('nombre') != None:
             try:
                 # Comprobamos si existe
-                objeto_actual = ObjetoBlog.objects.get(nombre=nombre_nuevo)
+                objeto_actual = ObjetoBlog.objects.get(
+                    nombre=paquete.configuracion('nombre'))
                 # Lo actualizamos
-                objeto_actual.titulo = titulo
+                objeto_actual.titulo = paquete.configuracion('titulo')
                 objeto_actual.codigo_servidor = codigo_servidor
                 objeto_actual.codigo = codigo
                 objeto_actual.save()
             except:
                 # Instalamos el nuevo objeto
                 objeto_nuevo = ObjetoBlog(
-                    titulo=titulo,
-                    nombre=nombre_nuevo,
-                    codigo_servidor=codigo_servidor,
-                    codigo=codigo)
+                    titulo=paquete.configuracion('titulo'),
+                    nombre=paquete.configuracion('nombre'),
+                    codigo_servidor=paquete.read('codigo.py'),
+                    codigo=paquete.read('codigo.htm'))
                 objeto_nuevo.save()
