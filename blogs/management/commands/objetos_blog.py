41cc1c81c740f6f0b21de5296a0094e6516d382a
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2012-08-04 20:53:22 +0100

    Eliminado metodo de extracción para usar un objeto de paquete Zip en su lugar.

--- blogs/management/commands/objetos_blog.py
+++ blogs/management/commands/objetos_blog.py
@@ -6,12 +6,11 @@ import zipfile
 from sew.ConfiguracionXML import ConfiguracionXML
 
 
-class Command(BaseCommand, ConfiguracionXML):
+class Command(BaseCommand):
     help = "Importa objetos de blog comprimidos en ZIP"
     args = "[archivo]"
 
     def handle(self, *args, **options):
-        titulo, nombre_nuevo, codigo_servidor, codigo = self._extraer(args[0])
         if nombre_nuevo != '':
             try:
                 # Comprobamos si existe
@@ -21,7 +20,6 @@ class Command(BaseCommand, ConfiguracionXML):
                 objeto_actual.codigo_servidor = codigo_servidor
                 objeto_actual.codigo = codigo
                 objeto_actual.save()
-                print 'Objeto actualizado'
             except:
                 # Instalamos el nuevo objeto
                 objeto_nuevo = ObjetoBlog(
@@ -30,25 +28,3 @@ class Command(BaseCommand, ConfiguracionXML):
                     codigo_servidor=codigo_servidor,
                     codigo=codigo)
                 objeto_nuevo.save()
-                print 'Instalado nuevo objeto ' + nombre_nuevo
-
-    def _extraer(self, ruta):
-        archivo_zip = open(ruta, 'rb')
-        objeto_zip = zipfile.ZipFile(archivo_zip)
-        codigo_servidor = ''
-        codigo = ''
-        titulo = ''
-        nombre = ''
-        self.cadena_configuracion_xml(objeto_zip.read('configuracion.xml'))
-        try:
-            codigo_servidor = objeto_zip.read('codigo.py')
-        except:
-            pass
-        try:
-            codigo = objeto_zip.read('codigo.htm')
-        except:
-            pass
-        archivo_zip.close()
-        titulo = self.valor_configuracion("titulo")
-        nombre = self.valor_configuracion("nombre")
-        return titulo, nombre, codigo_servidor, codigo
