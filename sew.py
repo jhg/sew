d5d843b51f5a4c6eb703e9911f7603cb3414b2d3
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2012-08-05 17:37:34 +0100

    Cambios en el script del asistente para ejecutarse aun sin instalar Django.

---
+++ sew.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+import os
+
+
+def _confirmado(pregunta):
+    respuesta = raw_input('\n  ¿' + pregunta + '? (S/N): ')
+    if respuesta == 'N' or respuesta == 'n':
+        return False
+    return True
+
+def _menu(titulo='Menu de opciones', *menu):
+    respuesta = False
+    while respuesta == False:
+        print ' '
+        print titulo
+        numero = 1
+        for opcion in menu:
+            print ' ' + str(numero) + ' -  ' + opcion
+            numero += 1
+        respuesta = raw_input('  Seleccione una opción: ')
+        try:
+            respuesta = int(respuesta)
+            if 0 < respuesta and respuesta <= len(menu):
+                break
+            else:
+                print ' No existe esa opción en el menu.'
+                respuesta = False
+        except:
+            respuesta = False
+    return respuesta - 1
+
+if __name__ == '__main__':
+    print 'Bienvenido al asistente de SEW.'
+    _opcion = _menu(
+        '¿Como esta ejecutando este asistente?',
+        'Es la primera vez para realizar la instalación',
+        'No es la primera vez, es una reparación',
+        'No estoy seguro')
+    if _opcion == 1:
+        _instalar = True
+    elif _opcion == 2:
+        _instalar = False
+    else:
+        _instalar = None
+    if _confirmado('Puedo eliminar archivos .pyc') \
+        and not _instalar:
+        print 'Eliminando archivos de bytecode de Python...'
+        os.system("find ./ -name \\*.pyc -exec rm {} \\;")
+        print 'Eliminados, ahora se generaran al ejecutar un .py otra vez'
