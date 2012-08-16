3c1bcd69e33b02e13a710b38248387e9d2e91f9e
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2012-08-16 09:43:25 +0100

    Definida etructura básica de los modelos de las recetas.

--- recetas/models.py
+++ recetas/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class ingrediente_receta(models.Model):
+    nombre = models.CharField(max_length=64, default="", blank=False)
+    # cantidad
+    # unidad
+    detalles = models.TextField(max_length=256, default="", blank=True)
+    def __unicode__(self):
+        return self.nombre
+
+class paso_elaboacion_receta(models.Model):
+    # numero de paso en receta
+    descripcion = models.TextField(max_length=1024, default="", blank=False)
+    def __unicode__(self):
+        return self.descripcion
+
+class receta(models.Model):
+    nombre = models.CharField(max_length=64, default="", blank=False)
+    descripcion = models.TextField(max_length=512, default="", blank=True)
+    creado = models.DateTimeField(auto_now_add=True)
+    modificado = models.DateTimeField(auto_now=True)
+    def __unicode__(self):
+        return self.nombre
