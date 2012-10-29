de6cd467c14c2f64069bed8e1691f0eb76850f5a
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2012-10-29 22:14:45 +0000

    Quitada traducción de tupla de opciones de modelo por producir errores.

--- adverts_fly/models.py
+++ adverts_fly/models.py
@@ -10,8 +10,8 @@ ADVERTS_FLY_DOMAINS = (
 
 # Type of advert in short link, view adf.ly API
 ADVERTS_FLY_TYPES = (
-    ('int', _('Interstitial')),
-    ('banner', _('Banner')),
+    ('int', 'Interstitial'),
+    ('banner', 'Banner'),
 )
 
 # Exclude domain of short link with advert
