664977b2e5af87660862e90e876be89f431b3669
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2013-02-03 22:18:43 +0000

    Configurado Django para servir archivos estaticos en desarrollo.

--- escritorios/urls.py
+++ escritorios/urls.py
@@ -1,5 +1,11 @@
 from django.conf.urls import patterns, url
+from django.conf import settings
 
 urlpatterns = patterns('escritorios.views',
     url(r'^$', 'escritorio', name='escritorio'),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^estaticos/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
