358b737ced1a8cd636b218ebfe1dfd4f3908b497
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2013-02-07 09:23:30 +0000

    Mejora de las plantillas de la administración usando la herencia.

--- admin/urls.py
+++ admin/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('admin.views',
     url(r'^$', 'index', name='index'),
+    url(r'^aplicaciones-django$', 'aplicaciones_django', name='aplicacionesdjango'),
 )
--- admin/views.py
+++ admin/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render_to_response
 from django.conf import settings
 
 def index(request):
-    return render_to_response("admin/base.htm", {'STATIC_URL': settings.STATIC_URL})
+    return render_to_response("admin/administracion.htm",
+      {
+        'STATIC_URL': settings.STATIC_URL,
+      })
+
+def aplicaciones_django(request):
+    return render_to_response("admin/aplicaciones-django.htm",
+    {
+        'STATIC_URL': settings.STATIC_URL,
+        'APPS_DJANGO': settings.INSTALLED_APPS,
+    })
