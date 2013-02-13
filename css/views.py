aa781f12acf0febf3db0ef80691c2bfd837e95b2
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2013-02-13 16:47:17 +0000

    Uso de cache en la preparación del diccionario de reglas de un CSS dinamico.

--- css/views.py
+++ css/views.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response
+from django.core.cache import cache
 from css.models import HojaCss
 
 
 def css(request, fcss):
     """ CSS dinamico """
-    hoja = HojaCss.objects.get(nombre=fcss)
-    reglas = {}
-    for selector in hoja.selectorcss_set.all():
-        declaraciones = {}
-        for declaracion in selector.declaracioncss_set.all():
-            declaraciones[declaracion.propiedad] = declaracion.valor
-        reglas[selector.valor] = declaraciones
+    reglas = cache.get('css-dinamico-reglas-' + fcss)
+    if reglas == None:
+        reglas = {}
+        hoja = HojaCss.objects.get(nombre=fcss)
+        for selector in hoja.selectorcss_set.all():
+            declaraciones = {}
+            for declaracion in selector.declaracioncss_set.all():
+                declaraciones[declaracion.propiedad] = declaracion.valor
+            reglas[selector.valor] = declaraciones
+        cache.set('css-dinamico-reglas-' + fcss, reglas, 600)
     return render_to_response("css/base.css",
       {
         'reglas': reglas,
